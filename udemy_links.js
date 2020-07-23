4dd79cbba329fe6da7ada15610daeab0011c9852
radarpok <vdiana.m8@gmail.com>
2020-07-23 16:40:45 +0200

    writing objects for saved udemy links waiting to be processed

---
+++ udemy_links.js
@@ -0,0 +1,191 @@
+module.exports.udemy_lessons = ()=>
+{
+    const mega_links = {
+
+    "20 ways to make money online": {
+        id: 1,
+        link: "https://mega.nz/#F!sJphka4K!NoetsVD7JxI_CRfeLO3Mvg",
+        duration_minutes: 67,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+        // file was deleted, reupload is needed
+    },
+
+    "Adobe Illustrator Advanced Vector Artwork": {
+        id: 2,
+        link: "https://mega.nz/#F!ZEhhXCBA!gr5LuybBX_M5yzbhmLztjw",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+        // file was deleted, reupload is needed
+    },
+
+    "After Effects CC Simple Character Animation": {
+        id: 3,
+        link: "https://mega.nz/#F!5AIAFYxD!LbJrXuA2I_kgvc0_DeLwtQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+        // file was deleted, reupload is needed
+    },
+
+    "Arduino Zero to Hero": {
+        id: 4,
+        link: "https://mega.nz/#F!IAIQRYgD!ylPKGr21LKhu3rrJUyZVQQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+        // file was deleted, reupload is needed
+    },
+
+    "Artificial Intelligence A-Z_ Learn How To Build An AI": {
+        id: 5,
+        link: "https://mega.nz/#F!UExRlAha!jAqa2YFcrcUWVnwbPzup8Q",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Become a Expert In Java _ Go Beginner To Advance": {
+        id: 6,
+        link: "https://mega.nz/#F!QcRm3IzZ!FYtNeiwSidSul2khTeIx2w",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Become a SuperLearner Learn Speed Reading And Advanced Memory": {
+        id: 7,
+        link: "https://mega.nz/#F!FEBVHSbT!N-WgpQ1S1LR8cT_gqRMsJQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Complete Python Course from Basics to Brilliance in HD": {
+        id: 8,
+        link: "https://mega.nz/#F!cZ4nHSDD!0i1S_hR5CEd2mbMfkm1f-Q",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Ethical Hacking With Python, JavaScript and Kali Linux": {
+        id: 9,
+        link: "https://mega.nz/#F!wFAQ0S7B!S7brzKAmqocBfDlX0soyyg",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Excel Beginners to Advance and Data Analytics": {
+        id: 10,
+        link: "https://mega.nz/#F!9cxRTQSK!u49iKoGoRhqggqnzNAak6w",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Learn Spanish audio CD": {
+        id: 11,
+        link: "https://mega.nz/#F!cNYmyIzD!-QB-8v0ItluXjszg0Q0faw",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Logo Design_ From Beginner To Professional": {
+        id: 12,
+        link: "https://mega.nz/#F!tYQwQAhL!37C09KPserSdHoFDPX7IsA",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Modern React with Redux 2019": {
+        id: 13,
+        link: "https://mega.nz/#F!hRxRCSjA!0v_lmRArIZebF5FS2XUcNQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Professional Video Editing School_Sony_Vegas_Pro": {
+        id: 14,
+        link: "https://mega.nz/#F!pFRRmaAa!XJnAHteDM5rJBRYjq7uLvw",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Project Management Professional (PMP) - 6th Edition": {
+        id: 15,
+        link: "https://mega.nz/#F!NAIkGI7B!CzHotC7c8J8-f_Dht4f7Ug",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "React for Absolute Beginners": {
+        id: 16,
+        link: "https://mega.nz/#F!4ZgRDYrR!djhDufHAiRVySOkAvoaEZA",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Statistics for Data Science and Business Analysis": {
+        id: 17,
+        link: "https://mega.nz/#F!YUB2hICQ!9CWLUkfeeRGcPkiALmaLWQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "The Complete ASP.NET MVC 5 Course": {
+        id: 18,
+        link: "https://mega.nz/#F!INoDEYaT!VuywG0ng5PtSXvnI7xSecg",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "The Complete Ubuntu Linux Server Administration Course": {
+        id: 19,
+        link: "https://mega.nz/#F!ZEIymIJA!9rmz7ZUpaRnpHuXrw-SNxQ",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+
+    "Ultimate Photoshop Training From Beginner to Pro": {
+        id: 20,
+        link: "https://mega.nz/#F!MBACQYrQ!TBZwsiM9S-oxKZW3e-5HGg",
+        duration_minutes: x,
+        processed_minutes: 0,
+        number_of_lessons: x,
+        processed_lessons: 0
+    },
+}
+
+    export default mega_links;
+};
